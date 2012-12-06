2bb26f7ef8119f053af8797aa0e7aa27a2af8cb2
John McLear <john@mclear.co.uk>
2012-12-06 21:46:35 +0000

    remember to add route for index page

--- server.js
+++ server.js
@@ -39,10 +39,17 @@ app.configure('production', function(){
 });
 
 // ROUTES
+// Index page
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/src/static/html/index.html');
+});
+
+// Drawings
 app.get('/d/*', function(req, res){
   res.sendfile(__dirname + '/src/static/html/draw.html');
 });
 
+// Static files IE Javascript and CSS
 app.use("/static", express.static(__dirname + '/src/static'));
 
 // LISTEN FOR REQUESTS
