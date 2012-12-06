d185cac1b273e5454edaba40aae8a8e0155b53c3
John McLear <john@mclear.co.uk>
2012-12-06 21:34:52 +0000

    Update src/static/js/canvas.js

--- src/static/js/canvas.js
+++ src/static/js/canvas.js
@@ -7,12 +7,12 @@ var socket = io.connect('/');
 
 // Random User ID
 // Used when sending data
-var uid =  (function() {
-     var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-} () );
+var uid = (function () {
+  var S4 = function () {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}());
 
 
 
@@ -24,23 +24,23 @@ var path_to_send = {};
 var active_color_rgb;
 var active_color_json = {};
 var $opacity = $('#opacity');
-var update_active_color = function() {
+var update_active_color = function () {
 
-    var rgb_array =  $('.active').attr('data-color').split(',');
-    var red = rgb_array[0] / 255;
-    var green = rgb_array[1] / 255;
-    var blue = rgb_array[2] / 255;
-    var opacity =  $opacity.val() / 255;
+  var rgb_array = $('.active').attr('data-color').split(',');
+  var red = rgb_array[0] / 255;
+  var green = rgb_array[1] / 255;
+  var blue = rgb_array[2] / 255;
+  var opacity = $opacity.val() / 255;
 
-    active_color_rgb =  new RgbColor( red, green, blue, opacity );
-    active_color_rgb._alpha = opacity;
+  active_color_rgb = new RgbColor(red, green, blue, opacity);
+  active_color_rgb._alpha = opacity;
 
-    active_color_json = {
-        "red" : red,
-        "green" : green,
-        "blue" : blue,
-        "opacity" : opacity
-    };
+  active_color_json = {
+    "red": red,
+    "green": green,
+    "blue": blue,
+    "opacity": opacity
+  };
 
 };
 
@@ -65,75 +65,75 @@ var timer_is_active = false;
 
 function onMouseDown(event) {
 
-    var point = event.point;
+  var point = event.point;
 
-    path = new Path();
-    path.fillColor = active_color_rgb;
-    path.add(event.point);
-    view.draw();
+  path = new Path();
+  path.fillColor = active_color_rgb;
+  path.add(event.point);
+  view.draw();
 
-    // The data we will send every 100ms on mouse drag
-    path_to_send = {
-        rgba : active_color_json,
-        start : event.point,
-        path : []
-    };
+  // The data we will send every 100ms on mouse drag
+  path_to_send = {
+    rgba: active_color_json,
+    start: event.point,
+    path: []
+  };
 
 
 }
 
 function onMouseDrag(event) {
-    
-    var step = event.delta / 2;
-    step.angle += 90;
-    
-    var top = event.middlePoint + step;
-    var bottom = event.middlePoint - step;
-    
-    path.add(top);
-    path.insert(0, bottom);
-    path.smooth();
-    view.draw();
 
-    // Add data to path
-    path_to_send.path.push({
-        top : top,
-        bottom : bottom
-    });
+  var step = event.delta / 2;
+  step.angle += 90;
 
-    // Send paths every 100ms
-    if ( !timer_is_active ) {
+  var top = event.middlePoint + step;
+  var bottom = event.middlePoint - step;
 
-        send_paths_timer = setInterval( function() {
+  path.add(top);
+  path.insert(0, bottom);
+  path.smooth();
+  view.draw();
 
-            socket.emit('draw:progress', uid, JSON.stringify(path_to_send) );
-            path_to_send.path = new Array();
+  // Add data to path
+  path_to_send.path.push({
+    top: top,
+    bottom: bottom
+  });
 
-        }, 100);
+  // Send paths every 100ms
+  if (!timer_is_active) {
 
-    }
+    send_paths_timer = setInterval(function () {
 
-    timer_is_active = true;
+      socket.emit('draw:progress', uid, JSON.stringify(path_to_send));
+      path_to_send.path = new Array();
+
+    }, 100);
+
+  }
+
+  timer_is_active = true;
 
 }
 
 
 function onMouseUp(event) {
-   
-    // Close the users path
-    path.add(event.point);
-    path.closed = true;
-    path.smooth();
-    view.draw();
 
-    // Send the path to other users
-    path_to_send.end = event.point;
-    socket.emit('draw:end', uid, JSON.stringify(path_to_send) );
+  // Close the users path
+  path.add(event.point);
+  path.closed = true;
+  path.smooth();
+  view.draw();
 
-    // Stop new path data being added & sent
-    clearInterval(send_paths_timer);
-    path_to_send.path = new Array();
-    timer_is_active = false;
+  // Send the path to other users
+  path_to_send.end = event.point;
+  socket.emit('draw:end', uid, JSON.stringify(path_to_send));
+
+  // Stop new path data being added & sent
+  clearInterval(send_paths_timer);
+  path_to_send.path = new Array();
+  timer_is_active = false;
 
 }
 
@@ -150,18 +150,18 @@ function onMouseUp(event) {
 // CONTROLS EVENTS
 
 var $color = $('.color');
-$color.on('click', function() {
+$color.on('click', function () {
 
-    $color.removeClass('active');
-    $(this).addClass('active');
+  $color.removeClass('active');
+  $(this).addClass('active');
 
-    update_active_color();
+  update_active_color();
 
 });
 
-$opacity.on('change', function() {
+$opacity.on('change', function () {
 
-    update_active_color();
+  update_active_color();
 
 });
 
@@ -179,32 +179,32 @@ $opacity.on('change', function() {
 // SOCKET.IO EVENTS
 
 
-socket.on('draw:progress', function( artist, data ) {
+socket.on('draw:progress', function (artist, data) {
 
-    // It wasnt this user who created the event
-    if ( artist !== uid && data ) {
+  // It wasnt this user who created the event
+  if (artist !== uid && data) {
 
-       progress_external_path( JSON.parse( data ), artist );
+    progress_external_path(JSON.parse(data), artist);
 
-    }
+  }
 
-}); 
-
-socket.on('draw:end', function( artist, data ) {
-
-    // It wasnt this user who created the event
-    if ( artist !== uid && data ) {
-       end_external_path( JSON.parse( data ), artist );
-    }
-
-}); 
-
-socket.on('user:connect', function(user_count) {
-    update_user_count( user_count );
 });
 
-socket.on('user:disconnect', function(user_count) {
-    update_user_count( user_count );
+socket.on('draw:end', function (artist, data) {
+
+  // It wasnt this user who created the event
+  if (artist !== uid && data) {
+    end_external_path(JSON.parse(data), artist);
+  }
+
+});
+
+socket.on('user:connect', function (user_count) {
+  update_user_count(user_count);
+});
+
+socket.on('user:disconnect', function (user_count) {
+  update_user_count(user_count);
 });
 
 
@@ -219,10 +219,11 @@ socket.on('user:disconnect', function(user_count) {
 // Updates the active connections
 var $user_count = $('#userCount');
 var $user_count_wrapper = $('#userCountWrapper');
-function update_user_count( count ) {
 
-    $user_count_wrapper.css('opacity', 1);
-    $user_count.text( (count === 1) ? " just you, why not invite some friends?" : " " + count );
+function update_user_count(count) {
+
+  $user_count_wrapper.css('opacity', 1);
+  $user_count.text((count === 1) ? " just you, why not invite some friends?" : " " + count);
 
 }
 
@@ -230,60 +231,58 @@ function update_user_count( count ) {
 var external_paths = {};
 
 // Ends a path
-var end_external_path = function( points, artist ) {
+var end_external_path = function (points, artist) {
 
-    var path = external_paths[artist];
+  var path = external_paths[artist];
 
-    if ( path ) {
+  if (path) {
 
-        // Close the path
-        path.add(points.end);
-        path.closed = true;
-        path.smooth();
-        view.draw();
+    // Close the path
+    path.add(points.end);
+    path.closed = true;
+    path.smooth();
+    view.draw();
 
-        // Remove the old data
-        external_paths[artist] = false;
+    // Remove the old data
+    external_paths[artist] = false;
 
-    }
+  }
 
 };
 
 // Continues to draw a path in real time
-progress_external_path = function( points, artist ) {
+progress_external_path = function (points, artist) {
 
 
-    var path = external_paths[artist];
+  var path = external_paths[artist];
 
-    // The path hasnt already been started
-    // So start it
-    if ( !path ) {
+  // The path hasnt already been started
+  // So start it
+  if (!path) {
 
-        // Creates the path in an easy to access way
-        external_paths[artist] = new Path();
-        path = external_paths[artist];
+    // Creates the path in an easy to access way
+    external_paths[artist] = new Path();
+    path = external_paths[artist];
 
-        // Starts the path
-        var start_point = new Point(points.start.x, points.start.y);
-        var color = new RgbColor( points.rgba.red, points.rgba.green, points.rgba.blue, points.rgba.opacity );
-        path.fillColor = color;
-        path.add(start_point);
+    // Starts the path
+    var start_point = new Point(points.start.x, points.start.y);
+    var color = new RgbColor(points.rgba.red, points.rgba.green, points.rgba.blue, points.rgba.opacity);
+    path.fillColor = color;
+    path.add(start_point);
 
-    }
+  }
 
-    // Draw all the points along the length of the path
-    var paths = points.path;
-    var length = paths.length;
-    for (var i = 0; i < length; i++ ) {
+  // Draw all the points along the length of the path
+  var paths = points.path;
+  var length = paths.length;
+  for (var i = 0; i < length; i++) {
 
-        path.add(paths[i].top);
-        path.insert(0, paths[i].bottom);
+    path.add(paths[i].top);
+    path.insert(0, paths[i].bottom);
 
-    }
+  }
 
-    path.smooth();
-    view.draw();
+  path.smooth();
+  view.draw();
 
 };
-
-
