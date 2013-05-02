a66d1f7ed57d344ec328088042082333ee628d7d
tranek <tranekmobile@gmail.com>
2013-05-01 22:45:04 -0400

    Added paper node module; ueberDB persistence
    Sketches are now persistent according to their room (saved to ueberDB;
    dirty database). There is a bug in the paperjs exportJSON() function
    that does not export the alpha channel of paths, so all saved sketches
    are at 100% opacity. I created an issue for this on the paperjs GitHub.

--- server.js
+++ server.js
@@ -4,14 +4,11 @@
 
 var express = require("express");
 var app = express();
-//var P = require('paper');
 var paper = require('paper');
 paper.setup(new paper.Canvas(1920, 1080));
-
 var socket = require('socket.io');
-//var paper = require('paper');
-//paper.setup(new paper.Canvas(1920, 1080));
-//var layer = paper.project.activeLayer;
+var ueberDB = require("ueberDB");
+var db = new ueberDB.database("dirty", {"filename" : "var/dirty.db"});
 
 app.configure(function(){
   app.use(express.static(__dirname + '/'));
@@ -67,7 +64,7 @@ var io = socket.listen(server);
 io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
-	disconnect(socket);
+    disconnect(socket);
   });
 
   // EVENT: User stops drawing something
@@ -80,7 +77,7 @@ io.sockets.on('connection', function (socket) {
   // EVENT: User stops drawing something
   // Having room as a parameter is not good for secure rooms
   socket.on('draw:end', function (room, uid, co_ordinates) {
-	io.sockets.in(room).emit('draw:end', uid, co_ordinates);
+    io.sockets.in(room).emit('draw:end', uid, co_ordinates);
     end_external_path(room, JSON.parse(co_ordinates), uid);
   });
   
@@ -91,7 +88,6 @@ io.sockets.on('connection', function (socket) {
   
 });
 
-//var papers = {};
 var projects = {};
 // Subscribe a client to a room
 function subscribe(socket, data) {
@@ -99,31 +95,41 @@ function subscribe(socket, data) {
 
   // Subscribe the client to the room
   socket.join(room);
-  
+
   // Create Paperjs instance for this room if it doesn't exist
-  // TODO try to load from database if it exists there
-  /*var paper = papers[room];
-  if (!paper) {
-    papers[room] = {};
-    //papers[room].paper = require('paper');
-    papers[room].paper = new P.PaperScope();
-    papers[room].paper.setup(new papers[room].paper.Canvas(1920, 1080));
-    papers[room].external_paths = {};
-  }*/
 
   var project = projects[room];
   if (!project) {
     projects[room] = {};
-	projects[room].project = new paper.Project(paper.view);
-	projects[room].external_paths = {};
+    projects[room].project = new paper.Project(paper.view);
+    projects[room].external_paths = {};
   }
-  
+  loadFromDB(room, socket);
+
   // Broadcast to room the new user count
   var active_connections = io.sockets.manager.rooms['/' + room].length;  
   io.sockets.in(room).emit('user:connect', active_connections);
  
 }
 
+// Try to load room from database
+function loadFromDB(room, socket) {
+
+  db.init(function (err) {
+    if(err) {
+      console.error(err);
+      process.exit(1);
+    }
+    db.get(room, function(err, value) {
+      console.log(value);
+	  if (value) {
+        projects[room].project.importJSON(value);
+        socket.emit('project:load', value);
+      }
+      db.close(function(){});
+    });
+  })
+}
 
 // When a client disconnects, unsubscribe him from
 // the rooms he subscribed to
@@ -143,20 +149,20 @@ function disconnect(socket) {
 // Unsubscribe a client from a room
 function unsubscribe(socket, data) {
   var room = data.room;
-  
+
   // Remove the client from socket.io room
   // This is optional for the disconnect event, we do it anyway
   // because we want to broadcast the new room population
   socket.leave(room);
-	
+
   // Broadcast to room the new user count
   if (io.sockets.manager.rooms['/' + room]) {
     var active_connections = io.sockets.manager.rooms['/' + room].length;  
     io.sockets.in(room).emit('user:disconnect', active_connections);
   } else {
-    // TODO if no one left in room, remove Paperjs instance
+    // Iff no one left in room, remove Paperjs instance
     // from the array to free up memory
-    console.log('Room is empty.');
+    projects[room] = false;
   }
   
 }
@@ -177,38 +183,25 @@ var end_external_path = function (room, points, artist) {
     path.closed = true;
     path.smooth();
     paper.view.draw();
-	
+
     // Remove the old data
-    //papers[room].external_paths[artist] = false;
     projects[room].external_paths[artist] = false;
 
   }
   
-  console.log("Room = " + room);
-  console.log("Project id = " + project.index);
-  debugger;
-  paper.view.exportFrames({
-    amount: 1,
-    directory: __dirname,
-    onComplete: function() {
-      console.log('Done exporting.');
-    },
-    onProgress: function(event) {
-      console.log(event.percentage + '% complete, frame took: ' + event.delta);
+  var json = project.exportJSON();
+  db.init(function (err) {
+    if(err) {
+      console.error(err);
+      process.exit(1);
     }
+    db.set(room, {project: json});
   });
-  
-  console.log(project.exportJSON());
-  
-  //project.exportSVG();
-
 };
 
 // Continues to draw a path in real time
 progress_external_path = function (room, points, artist) {
 
-  //var paper = papers[room].paper;
-  //var path = papers[room].external_paths[artist];
   var project = projects[room].project;
   project.activate();
   var path = projects[room].external_paths[artist];
@@ -218,10 +211,8 @@ progress_external_path = function (room, points, artist) {
   if (!path) {
 
     // Creates the path in an easy to access way
-    //papers[room].external_paths[artist] = new paper.Path();
-    //path = papers[room].external_paths[artist];
     projects[room].external_paths[artist] = new paper.Path();
-	path = projects[room].external_paths[artist];
+    path = projects[room].external_paths[artist];
 
     // Starts the path
     var start_point = new paper.Point(points.start[1], points.start[2]);
@@ -243,8 +234,6 @@ progress_external_path = function (room, points, artist) {
 
   path.smooth();
   paper.view.draw();
-  
-  console.log("project id = " + project.index);
 
 };
 
