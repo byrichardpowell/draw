d9ca5be9453adcda95e4643305106336343e6133
tranek <tranekmobile@gmail.com>
2013-05-03 00:50:35 -0400

    Fixed future additions not saving to db after clear

--- server.js
+++ server.js
@@ -268,14 +268,17 @@ function clearCanvas(room) {
   var project = projects[room].project;
   
   if (project && project.activeLayer && project.activeLayer.hasChildren()) {
-    // Remove all but the first layer
-    for (var i=0; i<paper.project.layers.length; i++) {
-      if (project.layers[i]._id > 1) {
-        project.layers[i].remove();
-        i--;
+    // Remove all but the active layer
+    if (project.layers.length > 1) {
+      var activeLayerID = project.activeLayer._id;
+      for (var i=0; i<project.layers.length; i++) {
+        if (project.layers[i]._id != activeLayerID) {
+          project.layers[i].remove();
+          i--;
+        }
       }
     }
-    // Remove all of the children from the first (active) layer
+    // Remove all of the children from the active layer
     if (paper.project.activeLayer && paper.project.activeLayer.hasChildren()) {
       paper.project.activeLayer.removeChildren();
     }
